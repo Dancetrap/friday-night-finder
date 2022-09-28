e51cde3f943106260f0ce675fa48452db1e95a46
Dancetrap <85971084+Dancetrap@users.noreply.github.com>
2022-09-27 21:09:22 -0400

    works with any letter

--- src/jsonResponse.js
+++ src/jsonResponse.js
@@ -9,6 +9,8 @@ const fs = require('fs');
 const json = fs.readFileSync(`${__dirname}/../characters.json`);
 
 const characters = JSON.parse(json);
+// const search = {};
+let search = [];
 const users = {};
 
 // Object with all character data
@@ -45,6 +47,8 @@ const respondJSONMeta = (request, response, status) => {
 };
 
 const getCharacters = (request, response, params) =>{
+    search = [];
+    
     if(!params.name){
         return respondJSON(request, response, 204, {
             message: "Type in search term"
@@ -52,10 +56,51 @@ const getCharacters = (request, response, params) =>{
         // return respondJSON(request, response, 200, params);
     }
 
+    // console.log(params.name);
+    // obj needs to be replaced by an array of items
+    // const characterArray = JSON.parse(characters);
+    let items = '';
+    let split = params.name.split("");
+
+    for(const k in characters)
+    {
+        // console.log(k);
+        let exist = true;
+        let name = k.split("");
+        for(let i = 0; i < params.name.length; i++)
+        {
+            if(exist)
+            {
+                if(name[i] != split[i])
+                {
+                    exist = false;
+                    break;
+                }
+            }
+        }
+        if(exist){
+            // console.log(characters[k]);
+            search.push(characters[k]);
+            // items += JSON.stringify(characters[k])
+        }
+
+    }
+        console.log(search);
+        const newJSON = {
+            ...search,
+        }
+
+    // console.log(newJSON);
     const obj = characters[params.name];
 
-    if(obj){
-        return respondJSON(request, response, 200, obj);
+    // if(newJSON != {}){
+    if(JSON.stringify(newJSON) != '{}'){
+        // console.log(newJSON.name);
+        for(const k in newJSON)
+        {
+            console.log(newJSON[k].name);
+        }
+        return respondJSON(request, response, 200, newJSON);
     }
     else
     {
